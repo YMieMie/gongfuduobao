5c76832c261efaa01e087ea041dd4f869400234e
YMieMie <ylin3253@163.com>
2016-09-18 19:39:37 +0800

    init

---
+++ gongfuduobao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gongfuduobao
+//
+//  Created by wangshanshan on 16/9/18.
+//  Copyright © 2016年 Magicsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gongfuduobao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gongfuduobao
+//
+//  Created by wangshanshan on 16/9/18.
+//  Copyright © 2016年 Magicsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
